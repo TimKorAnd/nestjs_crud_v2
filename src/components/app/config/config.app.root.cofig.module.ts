1921249118a943382ee0031087ebe41085fff509
Korniiets Artem <timkorand@gmail.com>
2021-07-29 18:23:34 +0300

    test gitignore

--- src/components/app/config/config.app.root.cofig.module.ts
+++ src/components/app/config/config.app.root.cofig.module.ts
@@ -1,6 +1,6 @@
 import { ConfigModule } from '@nestjs/config';
 
 export const configAppRootConfigModule = ConfigModule.forRoot({
-  envFilePath: `.env.${process.env.NODE_ENV || 'development.secret'}`, // change to development.local
+  envFilePath: `.env.${process.env.NODE_ENV || 'development.local'}`, // change to development.local
   isGlobal: true,
 });
